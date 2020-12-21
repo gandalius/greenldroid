a4547f9f6bb609b1bb963cf668dc23cbdd87380c
gandalivs <gandalivs@gmail.com>
2020-12-21 23:35:04 +0700

    prepare partisi baris dan kolom

--- app/src/main/java/livscode/tes/greenly/bi/Logic.java
+++ app/src/main/java/livscode/tes/greenly/bi/Logic.java
@@ -1,7 +1,22 @@
 package livscode.tes.greenly.bi;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Logic {
 
-    public Logic() {
+    private ArrayList<String> listBoardingPassTxt;
+    public Logic(ArrayList<String> listBoardingPassTxt) {
+        this.listBoardingPassTxt = listBoardingPassTxt;
     }
+
+    public void partitionRow(){
+
+    }
+
+    public void partitionColumn(){
+
+    }
+    
+
 }
