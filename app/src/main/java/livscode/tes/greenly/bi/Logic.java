40119adbd2aaa4acba167697e6f73d3f749162cb
gandalivs <gandalivs@gmail.com>
2020-12-22 00:00:20 +0700

    build logic

--- app/src/main/java/livscode/tes/greenly/bi/Logic.java
+++ app/src/main/java/livscode/tes/greenly/bi/Logic.java
@@ -6,19 +6,39 @@ import java.util.List;
 public class Logic {
 
     private ArrayList<String> listBoardingPassTxt;
+
     public Logic(ArrayList<String> listBoardingPassTxt) {
         this.listBoardingPassTxt = listBoardingPassTxt;
     }
 
-    public void partitionRow(String code){
+    public void partitionRow(String singleinput) {
+        String code = singleinput.substring(1,7);
         int batasAwal = 0;
-        int batasAkhir=127;
+        int batasAkhir = 127;
+        char huruf;
 
+
+        for (int i = 0; i < code.length(); i++) {
+            huruf = code.charAt(i);
+
+
+        }
 
     }
 
-    public void partitionColumn(){
+    public void partitionColumn(String singleinput) {
+        String code = singleinput.substring(1,7);
+        int batasAwal = 0;
+        int batasAkhir = 7;
 
+        for (int i = 0; i < code.length(); i++) {
+            if(code.charAt(i)=='R'){
+
+            }else if(code.charAt(i)=='L'){
+
+            }
+
+        }
     }
 
 
